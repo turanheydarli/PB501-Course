17554703836d65d080da0009ab28c7edaccdac79
Turan Heydarli <turan.heydarli@code.edu.az>
2024-06-29 12:20:52 +0400

    task completed

--- Lesson_1/Task_1/app.js
+++ Lesson_1/Task_1/app.js
@@ -1,54 +1,112 @@
-const ExpenseType = {
-    All: 'All',
-    Transport: 'Transport',
-    Entertainment: 'Entertainment',
-    Food: 'Food',
-    Shopping: 'Shopping',
-    Education: 'Education',
-};
-
-const expenses = Object.fromEntries(Object.keys(ExpenseType).map(type => [type, []]));
+const expenses = {
+    'All': [],
+    'Transport': [],
+    'Entertainment': [],
+    'Food': [],
+    'Shopping': [],
+    'Education': [],
+}
 
 const filterElement = document.getElementById('filter-select');
 const expensesTableBody = document.querySelector('#expenses-table-body');
 
-filterElement.innerHTML = Object.keys(ExpenseType).map((value) => `<option  ${value == ExpenseType.All ? 'selected' : ''}>` + value + '</option>');
+const mostExpensiveExpense = document.querySelector('#most-expensive-expense');
+const mostExpensiveDay = document.querySelector('#most-expensive-day');
+const avarageExpense = document.querySelector('#avarage-expense');
+
+const createForm = document.querySelector('#create-form');
+const formSubmitButton = document.querySelector('#form-submit-button');
+const dayInput = document.querySelector('#day-input');
+const expenceTypeInput = document.querySelector('#expence-type-input');
+const amountInput = document.querySelector('#amount-input');
 
 
+formSubmitButton.addEventListener('click', performAdd);
+formSubmitButton.addEventListener('click', renderPage);
 
-document.addEventListener('DOMContentLoaded', renderExpensesTable);
-filterElement.addEventListener('change', renderExpensesTable);
+filterElement.addEventListener('change', renderPage);
 
-
-// expenseTypeElement.appendChild(`<select>${Object.keys(ExpenseType).map((value) => '<option>' + value + '</option>')}</select>`);
-
-addExpense(ExpenseType.Transport, 2, 100);
-addExpense(ExpenseType.Transport, 2, 100);
-addExpense(ExpenseType.Transport, 2, 100);
-addExpense(ExpenseType.Transport, 2, 100);
-addExpense(ExpenseType.Transport, 2, 100);
-addExpense(ExpenseType.Transport, 2, 100);
+document.addEventListener('DOMContentLoaded', renderPage);
 
 
-addExpense(ExpenseType.Education, 2, 100);
+addExpense('Transport', 1, 1);
+addExpense('Transport', 2, 1);
+addExpense('Transport', 3, 2);
+addExpense('Transport', 4, 1);
+addExpense('Transport', 5, 8);
+addExpense('Transport', 6, 8);
 
+
+addExpense('Entertainment', 3, 10);
+addExpense('Entertainment', 4, 10);
+addExpense('Entertainment', 5, 100);
+addExpense('Entertainment', 6, 89);
+
+addExpense('Shopping', 6, 30);
+
+addExpense('Food', 1, 15);
+addExpense('Food', 2, 12);
+addExpense('Food', 3, 20);
+addExpense('Food', 5, 10);
+addExpense('Food', 6, 5);
+
+addExpense('Education', 6, 37);
 
 console.log(getAllExpenses());
+
+console.log(getAvarageExpense());
+
+
+function performAdd(event) {
+    event.preventDefault()
+
+    let day = parseInt(dayInput.value);
+    let amount = parseFloat(amountInput.value);
+    let type = expenceTypeInput.value;
+
+    addExpense(type, day, amount)
+}
+
+function renderPage() {
+    renderMostExpensiveDay();
+    renderMostExpensiveExpense();
+    renderAvarageExpense();
+
+    renderExpensesTable();
+}
+
+function renderMostExpensiveExpense() {
+    const expense = getMostExpensiveExpense();
+
+    mostExpensiveExpense.innerHTML = `Most Expensive Expense: ${expense.expenseType}`;
+}
+
+function renderMostExpensiveDay() {
+    const expense = getMostExpensiveDayByType('All');
+
+    mostExpensiveDay.innerHTML = `Most Expensive Day: ${expense.day}`;
+}
+
+function renderAvarageExpense() {
+    const expense = getAvarageExpense();
+
+    avarageExpense.innerHTML = `Avarage expense: ${expense.toFixed(1)}`;
+}
 
 function renderExpensesTable() {
     const selectedType = filterElement.value;
     const filteredExpenses = getExpensesByType(selectedType);
 
-    let tableId = filteredExpenses.indexOf(expense) + 1;
-
     expensesTableBody.innerHTML = filteredExpenses.map(expense => {
+        let tableId = filteredExpenses.indexOf(expense) + 1;
+
         return `<tr>
             <td>${tableId}</td>
             <td>${expense.expenseType}</td>
             <td>${expense.day}</td>
             <td>${expense.amount}</td>
         </tr>`;
-    });
+    }).join('');;
 }
 
 function getAllExpenses() {
@@ -83,7 +141,7 @@ function addExpense(expenseType, day, amount) {
         'amount': amount
     };
 
-    if (!valiedateExpense(newExpense)) {
+    if (!validateExpense(newExpense)) {
         return null;
     }
 
@@ -91,28 +149,74 @@ function addExpense(expenseType, day, amount) {
 }
 
 function getExpensesByType(expenseType) {
-    
-    if (expenseType == ExpenseType.All) {
+
+    if (expenseType == 'All') {
         return getAllExpenses();
     }
 
     return expenses[expenseType];
 }
 
-function valiedateExpense(expense) {
+function getSumOfExpenses() {
+    let allExpenses = getAllExpenses();
 
+    let sum = 0;
+
+    allExpenses.forEach(expense => {
+        expense.amount += sum;
+    });
+
+    return sum;
+}
+
+function getMostExpensiveDayByType(expenseType) {
+    let filteredExpenses = getExpensesByType(expenseType);
+
+    let mostExpensiveExpense = filteredExpenses[0];
+
+    for (let index = 0; index < filteredExpenses.length; index++) {
+        const expense = filteredExpenses[index];
+        if (expense.amount > mostExpensiveExpense.amount) {
+            mostExpensiveExpense = expense;
+        }
+    }
+
+    return mostExpensiveExpense;
+}
+
+function getMostExpensiveExpense() {
+    let filteredExpenses = getExpensesByType('All');
+
+    let mostExpensiveExpense = filteredExpenses[0];
+
+    for (let index = 0; index < filteredExpenses.length; index++) {
+        const expense = filteredExpenses[index];
+        if (expense.amount > mostExpensiveExpense.amount) {
+            mostExpensiveExpense = expense;
+        }
+    }
+
+    return mostExpensiveExpense;
+}
+
+function getAvarageExpense() {
+    let allExpenses = getAllExpenses();
+
+    let sum = 0;
+
+    allExpenses.forEach(expense => {
+        sum += expense.amount;
+    });
+
+    return sum / allExpenses.length;
+}
+
+function validateExpense(expense) {
     if (expense.day < 1 || expense.day > 7) {
         return false;
     }
 
-    switch (expense.expenseType) {
-        case ExpenseType.Transport:
-            break;
-        case ExpenseType.Education:
-            break;
-        default:
-            break;
-    }
+    
 
     return true;
 }
